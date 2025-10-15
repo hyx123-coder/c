e26ddf149a634ce05fc140ea324bd5e29252bb85
hyx123-coder <742136882@qq.com>
2025-10-15 14:14:50 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,48 @@
+/*#include<stdio.h>
+int main()
+{ printf ("****************\n");
+printf("very good!\n");
+printf("******************\n");
+return 0;
+}*/
+
+
+/*#include<stdio.h>
+int main()
+{int a,b,sum,minus;
+    a-123;b=456;
+    sum=a+b;minus=a-b;
+    printf("a=b=%d\na-b=%d\n",sum,minus);
+    return 0;
+}*/
+
+
+/*#include <stdio.h>
+int main()
+{int a,b,c;
+a=3;
+c=1;
+printf("%d\n",(a=b)*c);
+return 0;
+}*/
+
+
+/*#include<stdio.h>
+#define  PI 3.14
+int main()
+{
+    float r,area;
+    printf("\n  Enter r value:");
+    scanf("%f",&r);
+    area=PI*r*r;
+    printf("\n area=%f\n",area);
+    return 0;
+}*/
+#include<stdio.h>
+int main()
+{int x,y;
+x=5;
+y=8;
+printf("\n%d,%d,%d\n",x,(x+5)*2,y);
+return 0;
+}
